f44ab21948223f456cdad3d0cdf439d1baaf82e3
Bohdan Tsaplia <b.tsaplia@gmail.com>
2022-02-08 00:10:01 +0200

    modify projectile class

--- Islander/Assets/_Project/Scripts/Environment/Projectile.cs
+++ Islander/Assets/_Project/Scripts/Environment/Projectile.cs
@@ -1,6 +1,4 @@
-using System;
 using Gisha.Islander.Player;
-using Photon.Pun;
 using UnityEngine;
 
 namespace Gisha.Islander.Environment
@@ -8,14 +6,7 @@ namespace Gisha.Islander.Environment
     public class Projectile : MonoBehaviour
     {
         public Transform Owner { get; set; }
-
-        private PhotonView _pv;
-
-        private void Awake()
-        {
-            _pv = GetComponent<PhotonView>();
-        }
-
+        
         private void Start()
         {
             Invoke("Destroy", 60f);
@@ -26,12 +17,7 @@ namespace Gisha.Islander.Environment
             if (other.collider.CompareTag("Player") && !Owner.IsChildOf(other.transform))
                 other.collider.GetComponent<PlayerController>().GetDamage(30);
 
-            Destroy();
-        }
-
-        private void Destroy()
-        {
-            PhotonNetwork.Destroy(gameObject);
+            Destroy(gameObject);
         }
     }
 }
