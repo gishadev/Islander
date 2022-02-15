9fae63cf01e164f818482448f085f78eecfd1a95
Bohdan Tsaplia <b.tsaplia@gmail.com>
2022-02-15 22:05:54 +0200

    update build UI to show build operation

--- Islander/Assets/_Project/Scripts/Core/Building/BuildingGUI.cs
+++ Islander/Assets/_Project/Scripts/Core/Building/BuildingGUI.cs
@@ -8,6 +8,7 @@ namespace Gisha.Islander.Core.Building
     public class BuildingGUI : MonoBehaviour
     {
         [SerializeField] private GameObject buildingPanel;
+        [SerializeField] private TMP_Text buildOperationText;
         [SerializeField] private TMP_Text resourcesToBuildText;
 
         public void ChangePanelVisibility(bool isVisible)
@@ -18,10 +19,11 @@ namespace Gisha.Islander.Core.Building
         public void UpdateGUI(ItemCreationData creationData)
         {
             string costText = String.Empty;
-            
+
             foreach (var resourceForCraft in creationData.Recipe.ResourcesForCreation)
                 costText += $"{resourceForCraft.Count} {resourceForCraft.ResourceType}, \n";
 
+            buildOperationText.text = $"BUILD: {creationData.Prefab.name}";
             resourcesToBuildText.text = costText;
         }
     }
