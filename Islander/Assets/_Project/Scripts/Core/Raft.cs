9f2587cd0373fb6b29f2ac86b8115e43ecd54068
Bohdan Tsaplia <b.tsaplia@gmail.com>
2022-03-04 22:55:57 +0200

    improve floating mechanic for rafts

--- Islander/Assets/_Project/Scripts/Core/Raft.cs
+++ Islander/Assets/_Project/Scripts/Core/Raft.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Gisha.Islander.Core
@@ -7,5 +8,17 @@ namespace Gisha.Islander.Core
         [SerializeField] private int level;
 
         public int Level => level;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+                other.transform.SetParent(transform);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+                other.transform.SetParent(null);
+        }
     }
 }
