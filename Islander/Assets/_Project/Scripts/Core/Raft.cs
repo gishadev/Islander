4d50a818db547c00ab83ca180dc413524ccee973
Bohdan Tsaplia <b.tsaplia@gmail.com>
2022-03-07 20:36:42 +0200

    fix bug with player extinction

--- Islander/Assets/_Project/Scripts/Core/Raft.cs
+++ Islander/Assets/_Project/Scripts/Core/Raft.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Gisha.Islander.Core
@@ -9,16 +10,31 @@ namespace Gisha.Islander.Core
 
         public int Level => level;
 
+        private List<Transform> _playersOnRaft = new List<Transform>();
+
+        private void OnDisable()
+        {
+            foreach (var playerTrans in _playersOnRaft)
+            {
+                if (playerTrans != null)
+                    playerTrans.SetParent(null);
+            }
+        }
+        
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
+            {
+                _playersOnRaft.Add(other.transform);
                 other.transform.SetParent(transform);
+            }
         }
 
         private void OnTriggerExit(Collider other)
         {
             if (other.CompareTag("Player"))
             {
+                _playersOnRaft.Remove(other.transform);
                 other.transform.SetParent(null);
                 other.transform.rotation = Quaternion.Euler(0, other.transform.rotation.eulerAngles.y, 0);
             }
