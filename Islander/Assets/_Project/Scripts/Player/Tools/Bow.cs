8cfac123a3d1844131b0e7afc05daf024a597d7e
Bohdan Tsaplia <b.tsaplia@gmail.com>
2022-03-07 21:35:44 +0200

    improve bow shooting into crosshair

--- Islander/Assets/_Project/Scripts/Player/Tools/Bow.cs
+++ Islander/Assets/_Project/Scripts/Player/Tools/Bow.cs
@@ -1,3 +1,4 @@
+using System;
 using Gisha.Islander.Environment;
 using UnityEngine;
 
@@ -12,8 +13,14 @@ namespace Gisha.Islander.Player.Tools
         public override void PrimaryUse(Vector3 origin, Vector3 direction, PlayerController owner)
         {
             var arrow = Instantiate(arrowPrefab, shootPoint.position, shootPoint.rotation);
+            var shootDir = direction;
+
+            // Finding hit point to shoot correctly into crosshair.
+            if (Physics.Raycast(origin, direction, out var hitInfo))
+                shootDir = (hitInfo.point - shootPoint.position).normalized;
+            
             arrow.GetComponent<Projectile>().Owner = transform;
-            arrow.GetComponent<Rigidbody>().AddForce(shootPoint.forward * shootForce, ForceMode.Impulse);
+            arrow.GetComponent<Rigidbody>().AddForce(shootDir * shootForce, ForceMode.Impulse);
         }
     }
 }
