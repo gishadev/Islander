baf8452b1deb93aeff308de10374efe0b7124b3f
Bohdan Tsaplia <b.tsaplia@gmail.com>
2022-03-15 22:13:44 +0200

    add bow force charging up

--- Islander/Assets/_Project/Scripts/Player/Tools/Bow.cs
+++ Islander/Assets/_Project/Scripts/Player/Tools/Bow.cs
@@ -1,5 +1,5 @@
-using System;
 using Gisha.Islander.Environment;
+using Gisha.Islander.UI;
 using UnityEngine;
 
 namespace Gisha.Islander.Player.Tools
@@ -8,18 +8,33 @@ namespace Gisha.Islander.Player.Tools
     {
         [SerializeField] private GameObject arrowPrefab;
         [SerializeField] private Transform shootPoint;
-        [SerializeField] private float shootForce;
+        [SerializeField] private float maxShootForce;
+
+        private float _charge;
 
         public override void PrimaryUse(Vector3 origin, Vector3 direction, PlayerController owner,
             InteractType interactType)
         {
+            if (interactType == InteractType.Press)
+                _charge = 0f;
+
+            if (interactType == InteractType.Hold)
+            {
+                _charge += Time.deltaTime / 2f;
+                _charge = Mathf.Clamp01(_charge);
+            }
+
             if (interactType == InteractType.Release)
             {
                 var arrow = Instantiate(arrowPrefab, origin, shootPoint.rotation);
 
                 arrow.GetComponent<Projectile>().Owner = transform;
-                arrow.GetComponent<Rigidbody>().AddForce(direction * shootForce, ForceMode.Impulse);
+                arrow.GetComponent<Rigidbody>().AddForce(direction * maxShootForce * _charge, ForceMode.Impulse);
+
+                _charge = 0f;
             }
+
+            ProgressCircle.Instance.SetProgress(_charge);
         }
     }
 }
